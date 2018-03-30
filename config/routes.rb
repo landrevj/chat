4147b5ff7a8fa33e65f5e88e02311874e015c2d2
Joey Landreville <16bitbow@gmail.com>
2018-03-30 01:08:44 -0700

    Alias child_posts routes to 'posts'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :child_posts
+  resources :child_posts, :path => 'posts'
   resources :root_posts, :path => 'threads'
-
+  
   root to: redirect('/threads')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
