67f1d9404d9cc53c54112909d2989ca264afe61e
Joey Landreville <16bitbow@gmail.com>
2018-03-31 12:55:08 -0700

    Change card width. Validate body length.

--- app/models/child_post.rb
+++ app/models/child_post.rb
@@ -1,8 +1,9 @@
 class ChildPost < ApplicationRecord
   validates :user_id, presence: true
+  validates :body, presence: true, length: { maximum: 100, too_long: "cannot be longer than %{count} characters" }
 
   mount_uploader :picture, PictureUploader
-  
+
   belongs_to :root_post
   belongs_to :user
 end
--- app/models/root_post.rb
+++ app/models/root_post.rb
@@ -1,6 +1,7 @@
 class RootPost < ApplicationRecord
   validates :user_id, presence: true
-  
+  validates :body, presence: true, length: { maximum: 100, too_long: "cannot be longer than %{count} characters" }
+
   mount_uploader :picture, PictureUploader
 
   belongs_to :user
