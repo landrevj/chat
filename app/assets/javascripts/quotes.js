78c44e07e2d5aba8b5066e48f0ddd9850ca17bbe
Joey Landreville <16bitbow@gmail.com>
2018-04-20 18:02:01 -0700

    Add quoted post embedding on click.

---
+++ app/assets/javascripts/quotes.js
@@ -0,0 +1,96 @@
+$(document).on('turbolinks:load', function () {
+    $('body').on('click', 'a.quote', function(e){
+        e.preventDefault()
+        var target = $(e.target);
+        
+        if (!target.next().hasClass('post'))
+        {
+            var url = '/';
+            var type = '';
+            var id = target.attr('id');
+            
+            if (target.hasClass('root-quote'))       type = 'root-post';
+            else if (target.hasClass('child-quote')) type = 'child-post';
+            
+            $.ajax({
+                type: "GET",
+                url: "/api/" + type + "/" + id,
+                dataType: "json",
+                success: function (data) {
+                    target.after(build_post(type, data));
+                    console.log(data);
+                },
+                error: function (type, data) {
+                    console.log(data)
+                }
+            });
+        }
+        else
+        {
+            target.next().remove();
+        }
+    });
+});
+
+function build_post(type, data)
+{
+    var post = '',
+        sticky = '',
+        id = '',
+        subject = '',
+        edited = false,
+        picture_url = '',
+        picture_thumb_url = '';
+
+    if ('root_post' in data){
+        post = data.root_post;
+        id += '#' + post.id;
+        subject = `<div class="post-detail" id="subject" title="subject"><strong>` + post.subject + `</strong></div>`;
+    }
+    else if ('child_post' in data){
+        post = data.child_post;
+        id += post.id;
+    } 
+
+    if ('settings' in post && post.settings.sticky) sticky = 'sticky';
+
+    if (post.created_at != post.updated_at)
+    {
+    }
+
+    if (post.created_at != post.updated_at)
+    {
+        edited = `<div class="post-detail" id="timestamp-edited" title="` + post.updated_at + `"><strong>` + data.updated_ago + ` ago</strong></div>`;
+    }
+
+    if ('picture' in post)
+    {
+        picture_url = post.picture.url;
+        picture_thumb_url = post.picture.thumb.url;
+    }
+    
+    var result = `
+    <div class="`+ type +` post ` + sticky + ` embedded" id="` + type.split('_', 1) + `-` + post.id + `">
+        <a class="anchor" name="` + id + `"></a>
+        <div class="post-left-col">
+            <div class="post-details">
+                <div class="post-detail" id="user-name" title="username"><strong>` + data.user_name + `</strong> </div>
+                ` + (subject ? subject : '') + `
+                <div class="post-detail" id="id" title="id"><a><strong>#` + id + `</strong></a></div>
+                <div class="post-detail" id="timestamp-created" title="` + post.created_at + `"><strong>` + data.created_ago + ` ago </strong></div>
+                ` + (edited ? edited : '') + `
+            </div>
+            <div class="body"><p>` + data.markdown_body + `</p></div>
+        </div>
+
+        <div class="post-right-col">
+            <div class="post-image">
+                <a href="` + picture_url + `">
+                    <img src="` + picture_thumb_url + `">
+                </a>
+            </div>
+        </div>
+    </div>
+    `;
+    return result;
+}
