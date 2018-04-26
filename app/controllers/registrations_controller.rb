24eca38270e32331412c9aa35f2abc09a713f783
Joseph Landreville <landrevillejoseph@gmail.com>
2018-04-25 19:01:39 -0700

    Structure account page using bootstrap. (#5)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,23 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :set_user, only: [:preferences]
-
-  def preferences
-    render :preferences
-  end
-
-  def details
-    render :details
-  end
-
-  def threads
+  
+  def edit
     @stickies = current_user.root_posts.where('properties @> ?', {sticky: true}.to_json).order(created_at: :desc)
     @root_posts = current_user.root_posts.where('properties @> ?', {sticky: false}.to_json).order(created_at: :desc)
-    render :threads
-  end
-
-  def posts
     @child_posts = current_user.child_posts.order(created_at: :desc)
-    render :posts
   end
 
   protected 
