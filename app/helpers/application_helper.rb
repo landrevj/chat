447339b2e637580a48c35ef4267ce91e0bd27c2d
Joey Landreville <16bitbow@gmail.com>
2018-04-28 18:27:58 -0700

    Style alerts using rails keys aliased to bootstrap ones.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,12 @@ module ApplicationHelper
   require 'rouge'
   require 'rouge/plugins/redcarpet'
 
+  
   # Custom renderer for Redcarpet
   class HTML < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
     include Redcarpet::Render::SmartyPants
-
+    
     # Custom support for spoilers
     def block_quote(quote)
       if quote.gsub!(/(?<=<p>)\s*!\s*/, ' ')
@@ -16,62 +17,62 @@ module ApplicationHelper
         "<blockquote> #{quote}</blockquote>"
       end
     end
-
+    
     # youtube auto embedding from https://stackoverflow.com/questions/23051568/how-to-embed-a-youtube-video-in-markdown-with-redcarpet-for-rails
     def autolink(link, link_type)
       case link_type
-        when :url then url_link(link)
-        when :email then email_link(link)
+      when :url then url_link(link)
+      when :email then email_link(link)
       end
     end
-
+    
     def url_link(link)
       case link
         # regex from https://gist.github.com/afeld/1254889
-        when /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/ then youtube_link(link)
-        else normal_link(link)
+      when /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/ then youtube_link(link)
+      else normal_link(link)
       end
     end
-
+    
     def youtube_link(link)
       video_id = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/.match(link)[1]
       "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube-nocookie.com/embed/#{video_id}?rel=0\" frameborder=\"0\" allowfullscreen></iframe>"
     end
-
+    
     def normal_link(link)
       "<a href=\"#{link}\">#{link}</a>"
     end
-
+    
     def email_link(email)
       "<a href=\"mailto:#{email}\">#{email}</a>"
     end
   end
-
+  
   # Apply custom markdown here
   def custom_markdown(text)
     wrap_quotes(text)
   end
-
+  
   # Detect custom root and child post text references
   # @##12 matches root post ##12
   # @#12 matches child post #12
   def wrap_quotes(text)
     text.gsub(/@(#+)(\d+)/) do
-      begin
-        id = $2.to_i
-        if $1 == '##'
-          root_post = RootPost.find(id)
-          board = root_post.board.abbreviation
-          subject = current_user.id == root_post.user.id ? ' (OP | You)' : ' (OP)'
-          "<a class='quote root-quote' href='/#{board}/threads/#{$2}' id='#{id}'>@#{$1 + $2 + subject}</a>"
-        elsif $1 == '#'
-          child_post = ChildPost.find(id)
-          board = child_post.root_post.board.abbreviation
-          thread_id = child_post.root_post.id
-          subject = current_user.id == child_post.user.id ? ' (You)' : ''
-
-          "<a class='quote child-quote' href='/#{board}/threads/#{thread_id}##{$2}' id='#{id}'>@#{$1 + $2 + subject}</a>"
-        end
+    begin
+      id = $2.to_i
+      if $1 == '##'
+        root_post = RootPost.find(id)
+        board = root_post.board.abbreviation
+        subject = current_user.id == root_post.user.id ? ' (OP | You)' : ' (OP)'
+        "<a class='quote root-quote' href='/#{board}/threads/#{$2}' id='#{id}'>@#{$1 + $2 + subject}</a>"
+      elsif $1 == '#'
+        child_post = ChildPost.find(id)
+        board = child_post.root_post.board.abbreviation
+        thread_id = child_post.root_post.id
+        subject = current_user.id == child_post.user.id ? ' (You)' : ''
+        
+        "<a class='quote child-quote' href='/#{board}/threads/#{thread_id}##{$2}' id='#{id}'>@#{$1 + $2 + subject}</a>"
+      end
       rescue ActiveRecord::RecordNotFound
         "<a class='quote'>@#{$1 + $2} (Broken)</a>"
       end
@@ -86,7 +87,7 @@ module ApplicationHelper
       space_after_headers: true,
       safe_links_only: true,
     }
-
+    
     extensions = {
       autolink:           true,
       strikethrough:      true,
@@ -98,10 +99,10 @@ module ApplicationHelper
       fenced_code_blocks: true,
       disable_indented_code_blocks: true,
     }
-
+    
     renderer = HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
-
+    
     custom_markdown(markdown.render(text)).html_safe
   end
 
@@ -137,6 +138,15 @@ module ApplicationHelper
         end
       end
       r.html_safe
+    end
+  end
+
+  def flash_class(key)
+    case key
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-error"
+      when 'alert' then "alert alert-error"
     end
   end
 end
