380422c19f18cd38171cf88a1adf08a454a9ec27
Joey Landreville <16bitbow@gmail.com>
2018-04-29 16:21:04 -0700

    Configure Cable for Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.web_socket_server_url = "wss://powerful-retreat-75501.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://powerful-retreat-75501.herokuapp.com', 'http://powerful-retreat-75501.herokuapp.com']
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
