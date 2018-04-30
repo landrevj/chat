2d47f971608e8c470f1be8079e8a68bb6aee51e2
Joey Landreville <16bitbow@gmail.com>
2018-04-29 21:08:38 -0700

    Try removing completed migrations.

--- db/migrate/20180429223752_remove_room_id_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRoomIdFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference( :users, :rooms, index: true)
-  end
-end
--- db/migrate/20180429224100_remove_users_from_rooms.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUsersFromRooms < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference( :rooms, :user, index: true)
-  end
-end
