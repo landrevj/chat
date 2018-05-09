4a6eda0e2474b31b9cb2365e100c6e651136df7d
Joseph Landreville <landrevillejoseph@gmail.com>
2018-05-08 23:40:23 -0700

    Validate Room name.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,19 @@
 class Room < ApplicationRecord
   validates :user_id, presence: true
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { minimum: 1, maximum: 25, too_short: "cannot be blank", too_long: "cannot be longer than %{count} characters" }
+           
+  before_save :format_fields
+
   belongs_to :user
 
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
   has_many :messages, dependent: :destroy
+
+  private
+    def format_fields
+      self.name = self.name.parameterize.underscore
+    end
 end
