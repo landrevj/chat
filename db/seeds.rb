1ba2a410febead8287a9e45caeb3221dbe21cd7f
Joey Landreville <16bitbow@gmail.com>
2018-05-20 12:34:30 -0700

    Add some board seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+board_list = [
+  ['misc', 'General'],
+  ['meme', 'Funposting'],
+  ['auto', 'Automobiles'],
+  ['nat', 'Nature'],
+  ['tech', 'Technology'],
+  ['tv', 'Television'],
+  ['v', 'Video Games'],
+  ['vg', 'Video Game Generals'],
+  ['tg', 'Traditional Games'],
+  ['sci', 'Science'],
+  ['bis', 'Business'],
+  ['co', 'Comics'],
+  ['sp', 'Sports'],
+  ['ph', 'Photography'],
+  ['cook', 'Cooking'],
+  ['lit', 'Literature'],
+  ['mu', 'Music'],
+  ['fit', 'Fitness'],
+  ['news', 'Current News'],
+]
+
+board_list.each do |abbreviation, name|
+  Board.create(name: name, abbreviation: abbreviation)
+end
