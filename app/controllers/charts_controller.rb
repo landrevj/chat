a2013ed32a39df1fff6a4e30dd688f2c15220025
Joey Landreville <16bitbow@gmail.com>
2018-05-20 12:04:20 -0700

    Make the charts on home page a bit nicer.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,15 +1,18 @@
 class ChartsController < ApplicationController
-    def posts
-        render json: {"Total Live Threads": RootPost.all.count, "Total Live Posts": ChildPost.all.count}
-    end
-    def posts_by_hour
-        render json: [{name: 'Threads', data: RootPost.group_by_hour(:created_at).count},
-            {name: 'Posts', data: ChildPost.group_by_hour(:created_at).count}]
-    end
-    def messages
-        render json: Message.group_by_hour(:created_at).count
-    end
-    def rooms
-        render json: Room.all.map { |r| {"#{r.name}": r.messages.count}}.reduce(:merge)
-    end
+  def posts
+    render json: Board.all.map { |b| { name: b.abbreviation, data: { 'Threads' => b.root_posts.count, 'Posts' => ChildPost.joins(:root_post).where(root_posts: { board_id: b.id }).count } } }
+  end
+
+  def posts_by_hour
+    render json: [{ name: 'Root', data: RootPost.group_by_day(:created_at, range: 5.week.ago.midnight..Time.now).count },
+                  { name: 'Child', data: ChildPost.group_by_day(:created_at, range: 5.week.ago.midnight..Time.now).count }]
+  end
+
+  def messages
+    render json: Message.group_by_day(:created_at, range: 5.week.ago.midnight..Time.now).count
+  end
+
+  def rooms
+    render json: Room.all.map { |r| { "#{r.name}" => r.messages.count } }.reduce(:merge)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,43 @@
 class PagesController < ApplicationController
   def home
     @boards = Board.all.order(abbreviation: :asc)
+    @colors = ['#87AF5F', '#E0E07E', '#DC7C54', '#D05353', '#D66799', '#9F5ED1', '#7B53DF', '#4B76D5', '#4CB1DC', '#4CDCCF'].shuffle
+    @options = {
+      line: {
+        animation: {
+          duration: 200,
+          easing: 'easeInOutCubic',
+        },
+        tooltips: {
+          mode: 'index',
+          position: 'average',
+          intersect: false,
+          displayColors: true,
+        },
+        legend: {
+          position: 'bottom',
+        },
+      },
+      donut: {
+        animation: {
+          duration: 500,
+          easing: 'easeInOutCubic',
+        },
+      },
+      bar: {
+        animation: {
+          duration: 200,
+          easing: 'easeInOutCubic',
+        },
+        tooltips: {
+          mode: 'index',
+          position: 'average',
+          intersect: false,
+          displayColors: true,
+        },
+      },
+    }
+
     render template: "pages/#{params[:page]}"
   end
 
@@ -8,3 +45,5 @@ class PagesController < ApplicationController
     render template: "pages/#{params[:page]}"
   end
 end
+
+
