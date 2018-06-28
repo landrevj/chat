d53ded7b7d8fed97cbbd7d76f6344c6adbff065c
Joey Landreville <16bitbow@gmail.com>
2018-06-28 02:31:37 -0700

    Make account page cards dark.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :set_user, only: [:preferences]
-  
+
   def edit
-    @stickies = current_user.root_posts.where('properties @> ?', {sticky: true}.to_json).order(created_at: :desc)
-    @root_posts = current_user.root_posts.where('properties @> ?', {sticky: false}.to_json).order(created_at: :desc)
+    @stickies = current_user.root_posts.where('properties @> ?', { sticky: true }.to_json).order(created_at: :desc)
+    @root_posts = current_user.root_posts.where('properties @> ?', { sticky: false }.to_json).order(created_at: :desc)
     @child_posts = current_user.child_posts.order(created_at: :desc)
   end
 
-  protected 
+  protected
 
   # courtesy of https://www.mnishiguchi.com/2017/11/24/rails-devise-edit-account-without-password/
   def update_resource(resource, params)
-    return super if params["password"].present?
-    resource.update_without_password(params.except("current_password"))
+    return super if params['password'].present?
+    resource.update_without_password(params.except('current_password'))
   end
 
   private
